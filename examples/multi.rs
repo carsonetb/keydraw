1e28791563ea3a3d44c36bb828fd657b096a8128
carsonetb <blackhole243243@gmail.com>
2026-05-30 12:36:09 -0700

    example: multi-pipeline

---
+++ examples/multi.rs
@@ -0,0 +1,198 @@
+use keydraw::{
+    Command, DrawKey, Event, Program,
+    builders::{FragmentBuilder, PipelineBuilder, VertexBuilder},
+    data::{CameraUniform, Vertex},
+    run,
+    state::State,
+};
+use wgpu::util::DeviceExt;
+
+struct RectDrawer {
+    command: Command,
+}
+
+impl RectDrawer {
+    fn new(pipeline_id: u32, material_id: u32) -> Self {
+        Self {
+            command: Command {
+                key: DrawKey {
+                    z_index: 0,
+                    pipeline_id,
+                    material_id,
+                },
+                vertices: vec![
+                    Vertex {
+                        position: [0.0, 0.0, 0.0],
+                        color: [0.0, 0.0, 0.0, 0.0],
+                    },
+                    Vertex {
+                        position: [1.0, 0.0, 0.0],
+                        color: [0.0, 0.0, 0.0, 0.0],
+                    },
+                    Vertex {
+                        position: [0.0, 1.0, 0.0],
+                        color: [0.0, 0.0, 0.0, 0.0],
+                    },
+                    Vertex {
+                        position: [1.0, 1.0, 0.0],
+                        color: [0.0, 0.0, 0.0, 0.0],
+                    },
+                ],
+                indices: vec![0, 2, 3, 0, 3, 1],
+                instances: vec![],
+                stride: size_of::<f32>() as u32 * 8,
+            },
+        }
+    }
+
+    fn draw(&mut self, x: f32, y: f32, width: f32, height: f32, color: [f32; 4]) {
+        self.command.instances.append(
+            &mut bytemuck::bytes_of(&[x, y, width, height, color[0], color[1], color[2], color[3]])
+                .to_vec(),
+        );
+    }
+}
+
+struct Game {
+    pub camera_buffer: Option<wgpu::Buffer>,
+}
+
+impl Game {
+    fn new() -> Self {
+        Self {
+            camera_buffer: None,
+        }
+    }
+}
+
+impl Program for Game {
+    fn init(&mut self, state: &mut State) {
+        let camera_uniform = CameraUniform::new(800.0, 600.0);
+        let camera_buffer = state
+            .device
+            .create_buffer_init(&wgpu::util::BufferInitDescriptor {
+                label: Some("Camera Buffer"),
+                contents: bytemuck::cast_slice(&[camera_uniform]),
+                usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+            });
+
+        let camera_bind_group_layout =
+            state
+                .device
+                .create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                    entries: &[wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStages::VERTEX,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Uniform,
+                            has_dynamic_offset: false,
+                            min_binding_size: None,
+                        },
+                        count: None,
+                    }],
+                    label: Some("camera_bind_group_layout"),
+                });
+        let material = state.device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &camera_bind_group_layout,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: camera_buffer.as_entire_binding(),
+            }],
+            label: Some("camera_bind_group"),
+        });
+
+        let tri_shader = state
+            .device
+            .create_shader_module(wgpu::include_wgsl!("basic.wgsl"));
+        let tri_pipeline_layout =
+            state.create_simple_layout("Render Pipeline", &[&camera_bind_group_layout]);
+        let tri_pipeline = PipelineBuilder::new(
+            "Render Pipeline",
+            &tri_pipeline_layout,
+            &VertexBuilder::new(&tri_shader).with_simple_vertex_buffer(),
+            &FragmentBuilder::new(&tri_shader).with_replace_target(state),
+        )
+        .resolve(state);
+
+        let rect_shader = state
+            .device
+            .create_shader_module(wgpu::include_wgsl!("rect.wgsl"));
+        let rect_pipeline_layout =
+            state
+                .device
+                .create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                    label: Some("Render Pipeline Layout"),
+                    bind_group_layouts: &[&camera_bind_group_layout],
+                    immediate_size: 0,
+                });
+        let rect_pipeline = PipelineBuilder::new(
+            "Render Pipeline",
+            &rect_pipeline_layout,
+            &VertexBuilder::new(&rect_shader)
+                .with_simple_vertex_buffer()
+                .with_buffer(wgpu::VertexBufferLayout {
+                    array_stride: size_of::<f32>() as u64 * 8,
+                    step_mode: wgpu::VertexStepMode::Instance,
+                    attributes: &wgpu::vertex_attr_array![2 => Float32x4, 3 => Float32x4],
+                }),
+            &FragmentBuilder::new(&rect_shader).with_alpha_target(state),
+        )
+        .resolve(state);
+
+        state.pipeline_db.insert(0, rect_pipeline);
+        state.pipeline_db.insert(1, tri_pipeline);
+        state.material_db.insert(0, material);
+
+        self.camera_buffer = Some(camera_buffer);
+    }
+
+    fn render(&mut self) -> Vec<Command> {
+        let mut renderer = RectDrawer::new(0, 0);
+        renderer.draw(10.0, 10.0, 200.0, 200.0, [1.0, 1.0, 1.0, 0.5]);
+        renderer.draw(100.0, 100.0, 200.0, 200.0, [1.0, 1.0, 1.0, 0.5]);
+        vec![
+            renderer.command,
+            Command {
+                key: DrawKey {
+                    z_index: 0,
+                    pipeline_id: 1,
+                    material_id: 0,
+                },
+                vertices: vec![
+                    Vertex {
+                        position: [200.0, 100.0, 0.0],
+                        color: [1.0, 0.0, 0.0, 1.0],
+                    },
+                    Vertex {
+                        position: [100.0, 400.0, 0.0],
+                        color: [0.0, 1.0, 0.0, 1.0],
+                    },
+                    Vertex {
+                        position: [400.0, 400.0, 0.0],
+                        color: [0.0, 0.0, 1.0, 1.0],
+                    },
+                ],
+                indices: vec![0, 1, 2],
+                instances: vec![],
+                stride: 0,
+            },
+        ]
+    }
+
+    fn event(&mut self, event: &Event, state: &mut State) {
+        match event {
+            Event::Resize(width, height) => {
+                let camera = CameraUniform::new(*width as f32, *height as f32);
+                state.queue.write_buffer(
+                    &self.camera_buffer.as_ref().unwrap(),
+                    0,
+                    &bytemuck::cast_slice(&[camera]),
+                );
+            }
+        }
+    }
+}
+
+fn main() {
+    run(Box::new(Game::new())).unwrap();
+}
